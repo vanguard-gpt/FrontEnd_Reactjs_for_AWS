797c4be70695bc40ecb27168aa077f53fbd1149c
vanguard <vanguard09@naver.com>
2024-08-14 11:22:37 +0900

    components 추가

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
   plugins: [react()],
 
   server: {
     port: 3000
   },
+  //absolute path 
   resolve: {
     alias: {
       src: "/src",
